383d2232e68721e3529057918c39ecad07b69283
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-05-19 22:13:06 +0900

    ソースを追加

---
+++ src/iterable.ts
@@ -0,0 +1,31 @@
+function* generator(): Generator<number> {
+    let a = 0, b = 1;
+    while(true) {
+        yield a;
+        [a, b] = [b, a + b]
+    }
+}
+
+const gen = generator()
+for(let i=1; i<10; i++) {
+    console.log(gen.next().value)
+}
+
+
+const numbers = {
+    *[Symbol.iterator]() {
+        for (let n=1; n <= 10; n++) {
+            yield n;
+        }
+    }
+}
+
+for (let a of numbers) {
+    console.log(a);
+}
+
+let allNumbers = [...numbers];
+console.log(allNumbers);
+
+let [one, two, ...rest] = numbers;
+console.log(one, two, rest);
