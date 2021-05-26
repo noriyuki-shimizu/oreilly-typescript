cd1e29e112bfa097f4df8d2b409d1f73bb1affd1
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-05-26 23:29:19 +0900

    optional のソースを追加

--- src/type.ts
+++ src/type.ts
@@ -55,3 +55,40 @@ const userId = UserID('df567mn9u');
 
 queryForUser(userId)
 // queryForUser(companyId) // error
+
+// Optional
+interface Option<T> {
+    flatMap<U>(f: (value: T) => None): None
+    flatMap<U>(f: (value: T) => Option<U>): Option<U>
+    getOrElse(value: T): T
+}
+class Some<T> implements Option<T> {
+    constructor(private value: T) { }
+    flatMap<U>(f: (value: T) => None): None
+    flatMap<U>(f: (value: T) => Some<U>): Some<U>
+    flatMap<U>(f: (value: T) => Option<U>): Option<U> {
+        return f(this.value);
+    }
+    getOrElse(): T {
+        return this.value;
+    }
+}
+class None implements Option<never> {
+    flatMap(): None {
+        return this;
+    }
+    getOrElse<U>(value: U): U {
+        return value;
+    }
+}
+
+function Optional<T>(value: null | undefined): None
+function Optional<T>(value: T): Some<T>
+function Optional<T>(value: T): Option<T> {
+    if (value == null) {
+        return new None;
+    }
+    return new Some(value);
+}
+
+let result = Optional(null)
