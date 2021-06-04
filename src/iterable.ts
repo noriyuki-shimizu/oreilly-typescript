d97afbe0d0e934718f1d28fdc81c02d8e99e7755
清水紀行 <noriyuki.shimizu0612@gmail.com>
2021-06-04 21:38:59 +0900

    変数名を修正

--- src/iterable.ts
+++ src/iterable.ts
@@ -1,20 +1,20 @@
-function* generator(): Generator<number> {
+function* fib(): Generator<number> {
     let a = 0, b = 1;
-    while(true) {
+    while (true) {
         yield a;
         [a, b] = [b, a + b]
     }
 }
 
-const gen = generator()
-for(let i=1; i<10; i++) {
+const gen = fib()
+for (let i = 1; i < 10; i++) {
     console.log(gen.next().value)
 }
 
 
 const numbers = {
     *[Symbol.iterator]() {
-        for (let n=1; n <= 10; n++) {
+        for (let n = 1; n <= 10; n++) {
             yield n;
         }
     }
